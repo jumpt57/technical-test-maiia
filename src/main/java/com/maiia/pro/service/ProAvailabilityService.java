15fab4c69ae20b408c43a9a34bf7c9838098da01
Julien M. <jumpt57@users.noreply.github.com>
2023-12-20 01:45:27 +0000

    Implement generateAvailabilities

--- src/main/java/com/maiia/pro/service/ProAvailabilityService.java
+++ src/main/java/com/maiia/pro/service/ProAvailabilityService.java
@@ -1,6 +1,7 @@
 package com.maiia.pro.service;
 
 import com.maiia.pro.entity.Availability;
+import com.maiia.pro.entity.TimeSlot;
 import com.maiia.pro.repository.AppointmentRepository;
 import com.maiia.pro.repository.AvailabilityRepository;
 import com.maiia.pro.repository.TimeSlotRepository;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
 
 @Service
 public class ProAvailabilityService {
@@ -27,7 +31,34 @@ public class ProAvailabilityService {
     }
 
     public List<Availability> generateAvailabilities(Integer practitionerId) {
-        // TODO : implement this
-        return new ArrayList<>();
+
+        List<TimeSlot> timeSlots = timeSlotRepository.findByPractitionerId(practitionerId);
+
+        return timeSlots.stream()
+            .flatMap(timeSlot -> generateIntervals(timeSlot).stream())
+            .map(availability -> availabilityRepository.save(availability))
+            .collect(Collectors.toList());
+    }
+
+    private List<Availability> generateIntervals(TimeSlot timeSlot) {
+        List<Availability> availabilities = new ArrayList<>();
+        LocalDateTime current = timeSlot.getStartDate();
+
+        while (current.isBefore(timeSlot.getEndDate())) {
+
+            LocalDateTime newEndDate = current.plus(15, ChronoUnit.MINUTES);
+
+            Availability availability = Availability.builder()
+                .practitionerId(timeSlot.getPractitionerId())
+                .startDate(current)
+                .endDate(newEndDate)
+                .build();
+
+            availabilities.add(availability);
+
+            current = newEndDate;
+        }
+
+        return availabilities;
     }
 }
